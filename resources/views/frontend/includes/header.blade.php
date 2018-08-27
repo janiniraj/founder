7e8fa8fa8e3ce422e120f16513369a321d6cf393
EC2 Default User <ec2-user@ip-172-30-1-161.ap-southeast-1.compute.internal>
2018-08-27 04:11:00 +0000

    changes made by Umed

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -3,12 +3,12 @@
     <a href="#"><img src="{{url('/')}}/frontend/img/logo.png"></a>
   </div>
   <div class="menu">
-    <a href="../index.php" id="home_page_tab">Home</a>
-    <a href="profile.php" id="profile_page_tab">Profile</a>
-    <a href="recognition.php" id="recognition_page_tab">Recognition</a>
-    <a href="nation_building.php" id="nation_building_tab">Nation building</a>
-    <a href="foundation.php" id="foundation_page_tab">Foundation</a>
-    <a href="media.php" id="media_page_tab">Media</a>
+    <a href="{{url('/')}}" id="home_page_tab">Home</a>
+    <a href="{{url('/')}}/page/profile" id="profile_page_tab">Profile</a>
+    <a href="{{url('/')}}/page/recognition" id="recognition_page_tab">Recognition</a>
+    <a href="{{url('/')}}/page/nation_building" id="nation_building_tab">Nation building</a>
+    <a href="{{url('/')}}/page/foundation" id="foundation_page_tab">Foundation</a>
+    <a href="{{url('/')}}/page/media" id="media_page_tab">Media</a>
   </div>
 </header>
 
@@ -21,12 +21,12 @@
       <img class="mobile_home_logo" src="{{url('/')}}/frontend/img/logo.png">
       <ul class="ul_menu">    
         <div class="inner_menu">
-          <li><a id="home_page_tab_mobile" href="../index.php">Home</a></li>
-          <li><a id="profile_page_tab_mobile" href="profile.php">Profile</a></li>
-          <li><a id="recognition_page_tab_mobile" href="recognition.php">Recognition</a></li>
-          <li><a id="nation_building_tab_mobile" href="nation_building.php">Nation Building</a></li>
-          <li><a id="foundation_page_tab_mobile" href="foundation.php">Foundation</a></li>
-          <li><a id="media_page_tab_mobile" href="media.php">Media</a></li>
+          <li><a id="home_page_tab_mobile" href="{{url('/')}}">Home</a></li>
+          <li><a id="profile_page_tab_mobile" href="{{url('/')}}/page/profile">Profile</a></li>
+          <li><a id="recognition_page_tab_mobile" href="{{url('/')}}/page/recognition">Recognition</a></li>
+          <li><a id="nation_building_tab_mobile" href="{{url('/')}}/page/nation_building">Nation Building</a></li>
+          <li><a id="foundation_page_tab_mobile" href="{{url('/')}}/page/foundation">Foundation</a></li>
+          <li><a id="media_page_tab_mobile" href="{{url('/')}}/page/media">Media</a></li>
         </div>              
       </ul>        
     </div>
